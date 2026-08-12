155bcfac21fc5c4166dd269bab4f58f4ad0314d4
pshimizu <naohiko.shimizu@gmail.com>
2026-08-12 13:32:40 +0900

    演習用ローカルセットアップ

--- apps/C/mem/acc.c
+++ apps/C/mem/acc.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (C) 2026 Naohiko Shimizu <nshimizu@ip-arch.jp>
-#define CPUFREQ		900.0
+#define CPUFREQ		read_freerun_freq()
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,36 +8,23 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <stdint.h>
+#include "../read_counter.h"
 
-static inline uint32_t
-rdtsc32(void)
-{
-  volatile uint32_t pmccntr;
-  // Read the user mode perf monitor counter access permissions.
-      asm volatile("mrc p15, 0, %0, c9, c13, 0" : "=r"(pmccntr));
-//      asm volatile("mrc p15, 0, %0, c15, c12, 1" : "=r"(pmccntr));
-      return pmccntr;  
-}
 
 int main () {
   uint32_t start, end;
   volatile int *p,x;
 
-  asm volatile("mcr p15, 0, %0, c9, c12,0\n"::"r"(23));
-  asm volatile("mcr p15, 0, %0,c9, c12, 1\n"::"r"(0x8000000f));
-  asm volatile("mcr p15, 0, %0,c9, c12, 3\n"::"r"(0x8000000f));
-
-	  asm volatile("mcr p15, 0, %0, c9, c12,0\n"::"r"(23));
 	  p=(int*)mmap(NULL,100,PROT_READ|PROT_WRITE,
 		MAP_ANONYMOUS|MAP_PRIVATE,0,0);
-	  start=rdtsc32();
+	  start=read_freerun_counter();
 	  x=*p;
-	  end=rdtsc32();
-	  printf("1st READ: clock:%d, %fuS\n", (end-start), (double)(end-start)/CPUFREQ);
-	  start=rdtsc32();
+	  end=read_freerun_counter();
+	  printf("1st READ: clock:%d, %es\n", (end-start), (double)(end-start)/CPUFREQ);
+	  start=read_freerun_counter();
 	  x=*p;
-	  end=rdtsc32();
-	  printf("2nd READ: clock:%d, %fuS\n", (end-start), (double)(end-start)/CPUFREQ);
+	  end=read_freerun_counter();
+	  printf("2nd READ: clock:%d, %es\n", (end-start), (double)(end-start)/CPUFREQ);
 
   return 0;
 }
--- apps/C/mem/mmap.c
+++ apps/C/mem/mmap.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (C) 2026 Naohiko Shimizu <nshimizu@ip-arch.jp>
-#define CPUFREQ		900.0
+#define CPUFREQ		read_freerun_freq()
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,16 +9,7 @@
 #include <unistd.h>
 #include <stdint.h>
 
-static inline uint32_t
-rdtsc32(void)
-{
-  volatile uint32_t pmccntr;
-  // Read the user mode perf monitor counter access permissions.
-      asm volatile("mrc p15, 0, %0, c9, c13, 0" : "=r"(pmccntr));
-//      asm volatile("mrc p15, 0, %0, c15, c12, 1" : "=r"(pmccntr));
-      return pmccntr;  
-}
-
+#include "../read_counter.h"
 #ifndef START
 #define START 0x20001000
 #define LENGTH 0x1000
@@ -32,11 +23,11 @@ int main() {
   area1=(char *)mmap((void *)0x20000000,0x1000,PROT_WRITE,MAP_PRIVATE|MAP_ANONYMOUS, 0, 0);
   area2=(char *)mmap((void *)0x20004000,0x2000,PROT_WRITE,MAP_PRIVATE|MAP_ANONYMOUS, 0, 0);
 
- st=rdtsc32();
+ st=read_freerun_counter();
   area3=(char *)mmap((void *)START,LENGTH,PROT_WRITE,MAP_PRIVATE|MAP_ANONYMOUS, 0, 0);
- et=rdtsc32();
+ et=read_freerun_counter();
 
- printf("mmap time %f uS\n", (double)(et-st)/(CPUFREQ));
+ printf("mmap time %e s\n", (double)(et-st)/(CPUFREQ));
  printf("area1=%p, area2=%p, area3=%p\n",area1,area2,area3);
 
  return 0;
