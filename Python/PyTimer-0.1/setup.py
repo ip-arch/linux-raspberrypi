af2a9bfa60a1762c468a5a6bc4e9bfc478e074ef
pshimizu <naohiko.shimizu@gmail.com>
2026-08-26 10:22:41 +0900

    64bit adopt, update tests

--- Python/PyTimer-0.1/setup.py
+++ Python/PyTimer-0.1/setup.py
@@ -1,24 +1,5 @@
-"""
-Copyright (c) 2015-2023 Naohiko Shimizu
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2026 Naohiko Shimizu <nshimizu@ip-arch.jp>
 
 from distutils.core import setup, Extension
 
@@ -33,7 +14,7 @@ classifiers = ['Development Status :: 5 - Production/Stable',
                'Topic :: System :: Hardware']
 module = Extension ('PyTimer',
 		libraries = ['rt'],
-		sources = ['source/py_timer.c',  'source/constants.c'])
+		sources = ['PyTimer/py_timer.c',  'PyTimer/constants.c'])
 
 setup(name             = 'PyTimer',
       version          = '0.1',
--- Python/PyTimer-0.1/test/test.py
+++ Python/PyTimer-0.1/test/test.py
@@ -1,25 +1,7 @@
-#!/usr/bin/env python2.7
-"""
-Copyright (c) 2015 Naohiko Shimizu
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2026 Naohiko Shimizu <nshimizu@ip-arch.jp>
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
 
 import sys
 import time
@@ -44,7 +26,7 @@ class TestIntervalTimer(unittest.TestCase):
     def test_create_type(self):
         """Test illeagal argument for create"""
         with self.assertRaises(ValueError):
-            t0 = PyTimer.create(0)
+            t0 = PyTimer.create(-1)
 
     def test_over_delete(self):
         """Test delete already deleted timer"""
